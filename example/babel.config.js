cdd845bf0e433135379dc55f6bcf0dc05ca91882
rejaul karim <devrejaul.official@gmail.com>
2024-07-31 15:51:06 +0600

    fix lint babel

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const { getConfig } = require('react-native-builder-bob/babel-config');
 const pkg = require('../package.json');
@@ -5,12 +7,11 @@ const pkg = require('../package.json');
 const root = path.resolve(__dirname, '..');
 
 module.exports = function (api) {
-  api.cache(true);
-
-  return getConfig(
-    {
-      presets: ['babel-preset-expo'],
-    },
-    { root, pkg }
-  );
+ api.cache(true);
+ return getConfig(
+  {
+   presets: ['babel-preset-expo'],
+  },
+  { root, pkg }
+ );
 };
