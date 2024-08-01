f92e017dbd3b99a3e91a44a2189ceec63839dd7a
rejaul karim <devrejaul.official@gmail.com>
2024-08-02 01:35:08 +0600

    fix: path tw config

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-const root = path.resolve(__dirname, '..');
 
 module.exports = {
  // NOTE: Update this to include the paths to all of your component files.
- content: [`${root}**/*.{js,jsx,ts,tsx}`],
+ content: ['./src**/*.{js,jsx,ts,tsx}'],
  presets: [require('nativewind/preset')],
  theme: {
   extend: {},
