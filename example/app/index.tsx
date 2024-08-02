1dc1c62160667d4afe182f20e3b8e5ea629c3475
rejaul karim <devrejaul.official@gmail.com>
2024-08-02 23:12:45 +0600

    fix: lint problems

--- example/app/index.tsx
+++ example/app/index.tsx
@@ -2,7 +2,9 @@ import '../global.css';
 import { Text, View} from 'react-native';
 
 export default function App() {
- return (<View className='flex-1 justify-center items-center'>
-  <Text className='text-red-500'>Hello World</Text>
- </View>); 
+ return (
+  <View className='flex-1 justify-center items-center'>
+   <Text className='text-red-500'>Hello World</Text>
+  </View>
+ ); 
 }
