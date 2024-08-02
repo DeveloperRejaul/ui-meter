35b5a64cdc581d3282a94454e63fd5f44c387f26
rejaul karim <devrejaul.official@gmail.com>
2024-08-02 23:42:29 +0600

    add

--- example/app/index.tsx
+++ example/app/index.tsx
@@ -1,10 +1,9 @@
-import '../global.css';
-import { Text, View} from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function App() {
  return (
-  <View className='flex-1 justify-center items-center'>
-   <Text className='text-red-500'>Hello World</Text>
+  <View>
+   <Text>index</Text>
   </View>
- ); 
+ );
 }
